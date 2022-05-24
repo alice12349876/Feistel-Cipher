da681d04973ca845c51c32d8c12058f2dc6afd6e
siyingd1 <sding20@stuy.edu>
2022-05-24 15:31:58 -0400

    updated encoder

--- code/encoder.py
+++ code/encoder.py
@@ -1,8 +1,9 @@
 import random
 
 def generateKey(p):
-    num = bin(random.randint(0,2^p - 1))
+    num = bin(random.randint(0,2**p - 1))
     num = num[2:]
     while len(num) != p:
         num = "0" + num
-print(generateKey(4))
+    return num
+# print(generateKey(4))
