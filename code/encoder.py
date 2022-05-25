e7bb2b2e8c0204c92aa3ac5fed33009667e3e92e
siyingd1 <sding20@stuy.edu>
2022-05-25 15:17:40 -0400

    encoder

--- code/encoder.py
+++ code/encoder.py
@@ -1,9 +1,20 @@
 import random
+import bitarray
+
+shift
 
 def generateKey(p):
-    num = bin(random.randint(0,2**p - 1))
-    num = num[2:]
-    while len(num) != p:
-        num = "0" + num
-    return num
+    pass
+
+    # may need modification
+    # num = bin(random.randint(0,2**p - 1))
+    # num = num[2:]
+    # while len(num) != p:
+    #     num = "0" + num
+    # keys.append(num)
+    # return num
 # print(generateKey(4))
+
+# for i in range(16):
+def initialPermutation(s):
+    bitarray.bitarray()
