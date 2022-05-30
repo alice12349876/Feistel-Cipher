74af4ee16a5170d8bd078baae9e4638a61b54927
syd1 <syding036@gmail.com>
2022-05-29 21:45:52 -0400

    Added key generation code

--- code/encoder.py
+++ code/encoder.py
@@ -7,10 +7,11 @@ import codecs
 #Key to convert
 #string -> bit string
 
+# code for f function
 def stringToBits(str):
     stringBit = ""
     for i in range(len(str)):
-        asciiValue = ord(str[i])  
+        asciiValue = ord(str[i])
         binValue = bin(asciiValue)[2:]
         for i in range(8 - len(binValue)):
             binValue = "0" + binValue
@@ -27,8 +28,8 @@ def bitsToString(str):
 def initialPermutation(stringBit):
     initialArray = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7]
     newString = ""
-    for int i in range(64):
-        newString = newString + stringBit[initialArray[i])]
+    for i in range(64):
+        newString = newString + stringBit[initialArray[i]]
     return newString
 
 #64 bit -> 64 bit
@@ -38,7 +39,7 @@ def round(stringBit, curRound):
 
 def ffunction(right, curRound):
     a = expansion(right)
-    a = a ^ generateKey(curRound)
+    a = a ^ generateKey(keyBit, curRound)
     a = sbox(a)
     a = straightPerm(a)
     return a
@@ -80,8 +81,42 @@ def straightPerm(a):
 def finalPermutation(stringBit):
     finalArray = [40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25]
     newString = ""
-    for int i in range(64):
-        newString = newString + stringBit[finalArray[i])]
+    for i in range(64):
+        newString = newString + stringBit[finalArray[i]]
     return newString
 
+# code for key generation
+def generateKey(keyBit, round):
+    a = parityDrop(keyBit)
+    a = shiftLeft(a, round)
+    a = compressionBox(a)
+    return a
 
+def parityDrop(keyBit):
+    k = ""
+    for i in range(len(keyBit)):
+        if (i%8 != 0):
+            k += keyBit[i]
+    permutatedKey = ""
+    pTable = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4]
+    for i in pTable:
+        permutatedKey += k[i]
+    return permutatedKey
+
+# round will range from 1 to 16
+def shiftLeft(keyBit, round):
+    left = keyBit[:28]
+    right = keyBit[28:]
+    roundTable = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
+    shift = roundTable[round-1]
+    # rotate the left and right key
+    newLeft = left[shift:] + left[0:shift]
+    newRight = right[shift:] + right[0:shift]
+    return newLeft + newRight
+
+def compressionBox(keyBit):
+    compressionTable = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
+    key = ""
+    for i in compressionTable:
+        key += keyBit[i]
+    return key
