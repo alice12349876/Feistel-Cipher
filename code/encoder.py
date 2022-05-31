e975241e53bc5c0adf805b2f6b7d50e082c78848
siyingd1 <98930727+siyingd1@users.noreply.github.com>
2022-05-31 11:19:51 -0400

    Update encoder.py

--- code/encoder.py
+++ code/encoder.py
@@ -141,7 +141,7 @@ def expansion(right):
 # 48 bit input -> 32 bit output
 def sbox(a):
     # a is a 48 bit input
-    # each S box corresponds to one group of 8-bit input
+    # each S box corresponds to one group of 6-bit input
     sboxArray = [[[14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7], [0,15,7,4,14,2,13,10,3,6,12,11,9,5,3,8], [4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0], [15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13]],
     [[15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10], [3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5], [0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15], [13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9]],
     [[10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8], [13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1], [13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7], [1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12]],
@@ -162,7 +162,7 @@ def sbox(a):
         while (len(b) < 4):
             b = "0" + b
         newString = newString + b
-        # 4 bit output for each group of 8-bit input
+        # 4 bit output for each group of 6-bit input
     return newString
 
 # straight permutation in each Feistel round
